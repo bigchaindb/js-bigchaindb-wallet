7b69d5f75d32e6251918bd6d9d0613931c12ffcc
getlarge <ed@getlarge.eu>
2020-12-14 16:07:46 +0100

    add secret in interface

--- packages/types/src/cipher.ts
+++ packages/types/src/cipher.ts
@@ -3,6 +3,8 @@ export type CipherType = 'symmetric' | 'asymmetric';
 export interface Cipher {
   name: string;
   type: CipherType;
+  sharedSecret?: Uint8Array;
+  secret?: Uint8Array;
 
   encrypt(json: Record<string, unknown>, secretOrSharedKey: Uint8Array, key?: Uint8Array): Promise<string>;
 
