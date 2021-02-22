13bb0d95d17028e7c320e59994a1e8a6455e95c2
getlarge <ed@getlarge.eu>
2021-02-22 09:17:20 +0100

    imp: add new unit tests and vectors from SLIP10

--- packages/key-derivation/test/key-derivation.spec.ts
+++ packages/key-derivation/test/key-derivation.spec.ts
@@ -1,14 +1,19 @@
 import {
   BigChainWallet,
   BIG_CHAIN_DERIVATION_PATH,
+  DerivatedKeyPair,
+  ED25519_CURVE,
   EncryptKeyPair,
   EncryptKeyPairFactory,
+  HARDENED_OFFSET,
   KeyDerivation,
   SignKeyPair,
   SignKeyPairFactory,
   uint8ArrayToHexString,
+  X25519_CURVE,
 } from '../src';
 import cases from './fixtures/cases';
+import vectors from './fixtures/vectors';
 
 const assertKeypair = (actualKeypair: SignKeyPairFactory, expectedPublicKey: string, expectedPrivateKey: string) => {
   expect(actualKeypair.publicKey('base58')).toEqual(expectedPublicKey);
@@ -17,11 +22,11 @@ const assertKeypair = (actualKeypair: SignKeyPairFactory, expectedPublicKey: str
 
 const assertChildKeypair = (
   actualKeypair: SignKeyPairFactory | EncryptKeyPairFactory,
-  expectedKeyPair: SignKeyPair | EncryptKeyPair,
+  expectedKeyPair: SignKeyPairFactory | EncryptKeyPairFactory,
 ) => {
   const { publicKey, privateKey } = expectedKeyPair;
-  expect(actualKeypair.publicKey('hex')).toEqual(uint8ArrayToHexString(publicKey));
-  expect(actualKeypair.privateKey('hex')).toEqual(uint8ArrayToHexString(privateKey));
+  expect(actualKeypair.publicKey('hex')).toEqual(publicKey('hex'));
+  expect(actualKeypair.privateKey('hex')).toEqual(privateKey('hex'));
 };
 
 const specTestCase = (num: number) => () => {
@@ -35,25 +40,39 @@ const specTestCase = (num: number) => () => {
     expect(masterKey).toEqual(testCase.parentKey);
   });
 
-  it('should derives expected child keys', () => {
+  it('should derives expected account level keys', () => {
     testCase.keyPairs.forEach(([publicKey, privateKey], index) => {
       const derivatedSignKeyPair = wallet.getDerivatedKeyPair('sign', { account: index });
+      // const derivatedEncryptKeyPair = wallet.getDerivatedKeyPair('encrypt', { account: index });
       assertKeypair(derivatedSignKeyPair, publicKey, privateKey);
+    });
+  });
 
+  it('should derives expected index level keys', () => {
+    testCase.keyPairs.forEach((_, index) => {
       const childIndex = 1;
+      const derivatedSignKeyPair = wallet.getDerivatedKeyPair('sign', { account: index });
       const derivatedChildSignKeyPair = BigChainWallet.getDerivatedKeyPair('sign', derivatedSignKeyPair, childIndex);
+
+      const derivatedEncryptKeyPair = wallet.getDerivatedKeyPair('encrypt', { account: index });
       const derivatedChildEncryptKeyPair = BigChainWallet.getDerivatedKeyPair(
         'encrypt',
-        derivatedSignKeyPair,
+        derivatedEncryptKeyPair,
+        // derivatedSignKeyPair,
         childIndex,
       );
-      const expectedChildKeyPair = KeyDerivation.derivePath(
-        `${BIG_CHAIN_DERIVATION_PATH}/${index}'/${childIndex}'/0'`,
-        seedHex,
-      );
+      // TODO: test encrypteKeyPair from sign key pair
 
-      assertChildKeypair(derivatedChildSignKeyPair, SignKeyPair.fromDerivatedKeyPair(expectedChildKeyPair));
-      assertChildKeypair(derivatedChildEncryptKeyPair, EncryptKeyPair.fromDerivatedKeyPair(expectedChildKeyPair));
+      const expectedSignKeyPair = SignKeyPair.fromDerivatedKeyPair(
+        KeyDerivation.derivePath(`${BIG_CHAIN_DERIVATION_PATH}/${index}'/0'/${childIndex}'`, seedHex, 'sign'),
+      ).factory();
+
+      const expectedEncryptKeyPair = EncryptKeyPair.fromDerivatedKeyPair(
+        KeyDerivation.derivePath(`${BIG_CHAIN_DERIVATION_PATH}/${index}'/0'/${childIndex}'`, seedHex, 'encrypt'),
+      ).factory();
+
+      assertChildKeypair(derivatedChildSignKeyPair, expectedSignKeyPair);
+      assertChildKeypair(derivatedChildEncryptKeyPair, expectedEncryptKeyPair);
     });
   });
 
@@ -62,16 +81,38 @@ const specTestCase = (num: number) => () => {
     expect(() => new EncryptKeyPair({})).toThrow(new TypeError('The "publicKey" property is required.'));
   });
 
-  it('should throw a type error when creating key pair from invalid derived key pair', () => {
+  it('should throw a type error when creating key pair from invalid derivated key pair', () => {
     const invalidChainCode = new Uint8Array();
     const invalidKey = new Uint8Array();
-    const derivationPath = `${BIG_CHAIN_DERIVATION_PATH}/${0}'/${0}'/0'`;
+    const derivationPath = `${BIG_CHAIN_DERIVATION_PATH}/0'/0'/0'`;
     expect(() =>
-      SignKeyPair.fromDerivatedKeyPair({ key: invalidKey, chainCode: invalidChainCode, derivationPath }),
+      SignKeyPair.fromDerivatedKeyPair({
+        key: invalidKey,
+        chainCode: invalidChainCode,
+        curve: X25519_CURVE,
+        derivationPath,
+        depth: 5,
+      }),
+    ).toThrow(new TypeError(`'curve' must be ${ED25519_CURVE}.`));
+
+    expect(() =>
+      SignKeyPair.fromDerivatedKeyPair({
+        key: invalidKey,
+        chainCode: invalidChainCode,
+        curve: ED25519_CURVE,
+        derivationPath,
+        depth: 5,
+      }),
     ).toThrow(new TypeError(`Key should be ${KeyDerivation.keyLength} bytes length`));
 
     expect(() =>
-      EncryptKeyPair.fromDerivatedKeyPair({ key: invalidKey, chainCode: invalidChainCode, derivationPath }),
+      EncryptKeyPair.fromDerivatedKeyPair({
+        key: invalidKey,
+        chainCode: invalidChainCode,
+        curve: ED25519_CURVE,
+        derivationPath,
+        depth: 5,
+      }),
     ).toThrow(new TypeError(`Key should be ${KeyDerivation.keyLength} bytes length`));
   });
 
@@ -186,9 +227,48 @@ const specTestCase = (num: number) => () => {
   });
 };
 
+const specTestVector = (num: number) => () => {
+  const testCase = vectors[num];
+  const { seed: seedHex, vector } = testCase;
+  let derivatedKeyPair = new KeyDerivation(seedHex).getMasterKey('sign');
+
+  it(`should create master key from seed - [Chain m]`, () => {
+    const { chainCode, key } = derivatedKeyPair;
+    expect(uint8ArrayToHexString(chainCode)).toEqual(testCase.chainCode);
+    expect(uint8ArrayToHexString(key)).toEqual(testCase.key);
+    expect(uint8ArrayToHexString(SignKeyPair.getPublicKey(key))).toEqual(testCase.publicKey);
+  });
+
+  vector.forEach((subVector) => {
+    it(`should create child key from ${subVector.path}`, () => {
+      const { chainCode, key } = KeyDerivation.derivePath(subVector.path, seedHex, 'sign');
+      derivatedKeyPair = KeyDerivation.childKeyDerivation(derivatedKeyPair, subVector.index, HARDENED_OFFSET);
+
+      expect({
+        index: subVector.index,
+        path: subVector.path,
+        key: uint8ArrayToHexString(derivatedKeyPair.key),
+        chainCode: uint8ArrayToHexString(derivatedKeyPair.chainCode),
+        publicKey: uint8ArrayToHexString(SignKeyPair.getPublicKey(derivatedKeyPair.key)),
+      }).toEqual(subVector);
+
+      expect({
+        index: subVector.index,
+        path: subVector.path,
+        key: uint8ArrayToHexString(key),
+        chainCode: uint8ArrayToHexString(chainCode),
+        publicKey: uint8ArrayToHexString(SignKeyPair.getPublicKey(key)),
+      }).toEqual(subVector);
+    });
+  });
+};
+
 describe('KeyDerivation', function () {
   describe('Test Case 1', specTestCase(0));
   describe('Test Case 2', specTestCase(1));
   describe('Test Case 3', specTestCase(2));
   describe('Test Case 4', specTestCase(3));
+  describe('Test Case 5 - Ed25519 - vector1', specTestVector(0));
+  describe('Test Case 6 - Ed25519 - vector2', specTestVector(1));
+
 });
